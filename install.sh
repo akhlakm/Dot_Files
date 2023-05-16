a9735ebbeeb8c37bdfbfd74495d2ce94d49ad97e
akhlakm <me@akhlakm.com>
2023-05-15 21:04:36 -0400

    Add miniconda installer, update bashrc as well

--- install.sh
+++ install.sh
@@ -160,5 +160,18 @@ swapfile() {
     free
 }
 
+miniconda() {
+    sudo apt install curl
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh
+    /bin/rm Miniconda3-latest-Linux-x86_64.sh
+    source ~/.bashrc
+    conda --version
+    conda update -n base conda
+    conda install -n base conda-libmamba-solver
+    conda config --set solver libmamba
+    echo "Done!"
+}
+
 
 "$@"
