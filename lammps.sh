0819b7a928c096fdfaf7c274f9c65199fd7882ad
akhlakm <me@akhlakm.com>
2023-06-05 13:32:00 -0400

    Add lammps script

---
+++ lammps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+version="2023.06a"
+
+echo
+echo "LAMMPS script (v$version) by Akhlak Mahmood"
+echo
+
+## INITIALIZATION
+## ===============================================================
+shopt -s extglob
+
+CWD=$(echo $(cd $(dirname $0); pwd))
+
+havecmd() {
+        type "$1" &> /dev/null
+}
+nocmd() {
+        type "$1" &> /dev/null
+        ex=$?; [[ $ex -ne 0 ]] || return 1
+}
+die() {
+	echo "ERROR - $2"
+	exit $1
+}
+
+task=$1; [[ -n $task ]] || die 1 "Please specify a task: $0 <task>"
+
+download() {
+    git clone --branch stable --depth 5 https://github.com/lammps/lammps.git lammps_official
+}
