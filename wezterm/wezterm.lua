daff86b48e01b3d20b0f99c99f254143ce696560
akhlakm <me@akhlakm.com>
2024-03-08 21:10:22 -0500

    Update wezrc

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,25 @@ config.default_prog = { 'bash', '-l' }
 
 -- config.color_scheme = 'AdventureTime'
 
+config.keys = {
+
+  	{	-- Close Tab
+    		key = 'DownArrow',
+    		mods = 'CMD',
+    		action = wezterm.action.CloseCurrentTab { confirm = false },
+  	},
+
+  	{	-- New Tab
+    		key = 'UpArrow',
+    		mods = 'CMD',
+    		action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+  	},
+
+	-- Tab navigation
+	{ key = 'LeftArrow', mods = 'CMD', action = wezterm.action.ActivateTabRelative(-1) },
+  	{ key = 'RightArrow', mods = 'CMD', action = wezterm.action.ActivateTabRelative(1) },
+
+}
 
 
 -- and finally, return the configuration to wezterm
