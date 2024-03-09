a6bd8d59fe9c79452624e4ed7ee48f0c2ec1ca56
akhlakm <me@akhlakm.com>
2024-03-09 01:53:52 -0500

    Add wez shortcut for panes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,35 +1,48 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- Shell
-config.default_prog = { 'bash', '-l' }
+config.default_prog = { "bash", "-l" }
 
--- config.color_scheme = 'AdventureTime'
+-- White colors
+-- config.color_scheme = "Material"
+
+-- Leader key
+config.leader = { key = "\\", mods = "CTRL", timeout_milliseconds = 1000 }
 
 config.keys = {
 
-  	{	-- Close Tab
-    		key = 'DownArrow',
-    		mods = 'CMD',
-    		action = wezterm.action.CloseCurrentTab { confirm = false },
-  	},
+	{ -- Close Tab
+		key = "DownArrow",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentTab({ confirm = false }),
+	},
 
-  	{	-- New Tab
-    		key = 'UpArrow',
-    		mods = 'CMD',
-    		action = wezterm.action.SpawnTab 'CurrentPaneDomain',
-  	},
+	{ -- New Tab
+		key = "UpArrow",
+		mods = "CMD",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
 
 	-- Tab navigation
-	{ key = 'LeftArrow', mods = 'CMD', action = wezterm.action.ActivateTabRelative(-1) },
-  	{ key = 'RightArrow', mods = 'CMD', action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "LeftArrow", mods = "CMD", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "RightArrow", mods = "CMD", action = wezterm.action.ActivateTabRelative(1) },
 
+	-- Split panes
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "=",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
 }
-
 
 -- and finally, return the configuration to wezterm
 return config
-
