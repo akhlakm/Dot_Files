2451a0fc473d4189faec8aad7e07babc4f06e0e3
akhlakm <me@akhlakm.com>
2024-03-10 12:15:03 -0400

    Add gitignore syntax highlighter

--- neovim/lua/kickstart/plugins/treesitter.lua
+++ neovim/lua/kickstart/plugins/treesitter.lua
@@ -7,7 +7,17 @@ return {
 
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "bash", "python", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+				ensure_installed = {
+					"bash",
+					"python",
+					"c",
+					"html",
+					"markdown",
+					"gitignore",
+					"lua",
+					"vim",
+					"vimdoc",
+				},
 				-- Autoinstall languages that are not installed
 				auto_install = true,
 				highlight = { enable = true },
