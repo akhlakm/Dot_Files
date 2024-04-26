bb55fab70dca79b92d26328587c96fb87e34a2b3
akhlakm <me@akhlakm.com>
2024-04-25 22:50:44 -0400

    Update neotree options

--- neovim/lua/plugins/neotree.lua
+++ neovim/lua/plugins/neotree.lua
@@ -14,6 +14,21 @@ return {
 					position = "left",
 					width = 30,
 				},
+				sources = {
+					"filesystem",
+				},
+				filesystem = {
+					filtered_items = {
+						visible = true,
+						hide_gitignored = false,
+						hide_hidden = false,
+						hide_dotfiles = false,
+					},
+					follow_current_file = {
+						enabled = true,
+					},
+					hijack_netrw_behavior = "open_default",
+				},
 			})
 		end,
 	},
