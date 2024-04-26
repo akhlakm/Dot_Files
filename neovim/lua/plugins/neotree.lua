7624d4a905b606a6c066b7acbfbe6d4b9f8bb6fe
akhlakm <me@akhlakm.com>
2024-04-25 22:37:08 -0400

    Update neotree width to 30

--- neovim/lua/plugins/neotree.lua
+++ neovim/lua/plugins/neotree.lua
@@ -8,5 +8,13 @@ return {
 			"MunifTanjim/nui.nvim",
 			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
+		config = function()
+			require("neo-tree").setup({
+				window = {
+					position = "left",
+					width = 30,
+				},
+			})
+		end,
 	},
 }
